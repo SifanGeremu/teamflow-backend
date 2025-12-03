0c59722ec3a0485b01891422f773dcd425bbe4cb
SifanGeremu <wins33975@gmail.com>
2025-12-03 22:12:25 +0300

    separated query from registration

--- github/forgit.js
+++ github/forgit.js
@@ -49,3 +49,18 @@ const handleLogin = (user) => {
 } else {
   console.log("you have user privileges");
 };
+ if (handleLogin(currentuser) === "admin") {
+  console.log("you have admin privileges");
+} else {
+  console.log("you have user privileges");
+};
+ if (handleLogin(currentuser) === "admin") {
+  console.log("you have admin privileges");
+} else {
+  console.log("you have user privileges");
+};
+ if (handleLogin(currentuser) === "admin") {
+  console.log("you have admin privileges");
+} else {
+  console.log("you have user privileges");
+};
